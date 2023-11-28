ee5dde64fcfa42ed4d86333d68ceea58a581b253
iavianm <iavianm@mail.ru>
2023-11-28 12:32:13 +0200

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,30 @@
 const jwt = require("jsonwebtoken");
 const Error401 = require("../errors/error401");
 const {
-    NODE_ENV,
-    JWT_SECRET_KEY,
-    JWT_SECRET_KEY_DEV,
+  NODE_ENV,
+  JWT_SECRET_KEY,
+  JWT_SECRET_KEY_DEV,
 } = require("../config/constants");
 
 const authMiddleware = (req, res, next) => {
-    try {
-        const token = req.cookies.jwt;
+  console.log(req);
+  try {
+    const token = req.cookies.jwt;
 
-        if (!token) {
-            throw new Error401("Токен отсутствует");
-        }
-
-
-        req.user = jwt.verify(token, NODE_ENV === "production" ? JWT_SECRET_KEY : JWT_SECRET_KEY_DEV);
-
-        next();
-    } catch (err) {
-        const error401 = new Error401("Токен недействителен");
-        next(error401);
+    if (!token) {
+      throw new Error401("Токен отсутствует");
     }
+
+    req.user = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET_KEY : JWT_SECRET_KEY_DEV,
+    );
+
+    next();
+  } catch (err) {
+    const error401 = new Error401("Токен недействителен");
+    next(error401);
+  }
 };
 
 module.exports = authMiddleware;
