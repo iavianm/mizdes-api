e71bb78b3f5837b16a32251513770e355c79e3ad
iavianm <iavianm@mail.ru>
2023-11-28 12:34:54 +0200

    fix

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,16 @@ const auth = require("../middlewares/auth");
 
 const router = express.Router();
 const {
-    updateUser,
-    getCurrentUser,
-    loginUser,
-    createUser,
-    logoutUser,
+  updateUser,
+  getCurrentUser,
+  loginUser,
+  createUser,
+  logoutUser,
 } = require("../controllers/users");
-const {
-    userLogin,
-    userCreate,
-    userUpdate,
-} = require("../utils/joiSchemes");
+const { userLogin, userCreate, userUpdate } = require("../utils/joiSchemes");
 
-router.post("/signin", celebrate(userLogin), loginUser);
-router.post("/signup", celebrate(userCreate), createUser);
+router.post("/api/signin", celebrate(userLogin), loginUser);
+router.post("/api/signup", celebrate(userCreate), createUser);
 router.get("/signout", auth, logoutUser);
 
 router.get("/users/me", auth, getCurrentUser);
